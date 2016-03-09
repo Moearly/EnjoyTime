b0f597f9af1b46696078b4a4f765d04dbea0d7a1
Moearly <982221982@qq.com>
2016-03-10 00:25:07 +0800

    数据库修改----添加默认数据

---
+++ app/src/main/java/com/martn/enjoytime/ui/activity/AddGoalActivity.java
@@ -0,0 +1,13 @@
+package com.martn.enjoytime.ui.activity;
+
+/**
+ * Title: EnjoyTime
+ * Package: com.martn.enjoytime.ui.activity
+ * Description: ("添加目标界面")
+ * Date 2014/10/5 21:02
+ *
+ * @author MartnLei MartnLei_163_com
+ * @version V1.0
+ */
+public class AddGoalActivity {
+}
